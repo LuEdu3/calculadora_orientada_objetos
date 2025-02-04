242516d0a28679d5f39c744ebbe5f31395d15e34
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2025-02-04 11:23:22 -0300

    declarado

---
+++ Iniciando_objetos/Menu.cs
@@ -0,0 +1,19 @@
+using System.Runtime.CompilerServices;
+
+namespace Menu
+{
+    public class Navegacao
+    {
+        public static string ImprimirMenu()
+        {
+            string opcao = "";
+            do
+            {
+                Console.WriteLine("\nCalculadora");
+                Console.WriteLine("(+) Adição");
+                Console.WriteLine("(+) Adição");
+            }
+            return opcao = Console.WriteLine();
+        }
+    }
+}
---
+++ Iniciando_objetos/Program.cs
@@ -0,0 +1,27 @@
+﻿int n1;
+int n2;
+
+Console.WriteLine("Calculadora: ");
+Console.Write("Digite o primeiro número");
+if(int.TryParse(Console.ReadLine(), out n1)){
+Console.Write("Digite o segundo número");
+if(int.TryParse(Console.ReadLine(), out n1)){
+
+Console.Write("Qual tipo de operação você quer: ");
+Console.WriteLine("(+) Adição");
+Console.WriteLine("(-) Subtração");
+Console.WriteLine("(*) Multiplicação");
+Console.WriteLine("(/) Divisão");
+}
+}
+// if(!int.TryParse(Console.ReadLine(),out int operacao))
+// {
+//     switch(operacao)
+//     {
+//         case '+':
+
+//         break;
+//     }
+// }
+// }
+// }
