353de9b26df3b7609630739c8dfa0257981e1585
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2025-02-05 07:55:46 -0300

    Feito o codigo para somas divisoes subtraçoes e multiplicações

--- Iniciando_objetos/Program.cs
+++ Iniciando_objetos/Program.cs
@@ -5,23 +5,35 @@ Console.WriteLine("Calculadora: ");
 Console.Write("Digite o primeiro número");
 if(int.TryParse(Console.ReadLine(), out n1)){
 Console.Write("Digite o segundo número");
-if(int.TryParse(Console.ReadLine(), out n1)){
+if(int.TryParse(Console.ReadLine(), out n2)){
 
 Console.Write("Qual tipo de operação você quer: ");
 Console.WriteLine("(+) Adição");
 Console.WriteLine("(-) Subtração");
 Console.WriteLine("(*) Multiplicação");
 Console.WriteLine("(/) Divisão");
-}
-}
-// if(!int.TryParse(Console.ReadLine(),out int operacao))
-// {
-//     switch(operacao)
-//     {
-//         case '+':
 
-//         break;
-//     }
-// }
-// }
-// }
+if(!int.TryParse(Console.ReadLine(),out int operacao))
+{
+    switch(operacao)
+    {
+        case '+':
+        Console.WriteLine($"O resultado da adição é: {n1+n2}");
+        break;
+    
+        case '-':
+        Console.WriteLine($"O resultado da subtração é: {n1-n2}");
+        break;
+
+        case '*':
+        Console.WriteLine($"O resultado da multiplicação é: {n1*n2}");
+        break;
+
+        case '/':
+        Console.WriteLine($"O resultado da operação é: {n1/n2}");
+        break;
+    }
+
+}
+}
+}
