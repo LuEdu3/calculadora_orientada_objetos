1a8dd7c912f309c6010195330b8555a5c318756a
Luiz Eduardo <luizinformaticaeduardo@gmail.com>
2025-02-05 07:51:04 -0300

    Comentei o menu.cs

--- Iniciando_objetos/Menu.cs
+++ Iniciando_objetos/Menu.cs
@@ -1,19 +1,18 @@
-using System.Runtime.CompilerServices;
+// using System.Runtime.CompilerServices;
 
-namespace Menu
-{
-    public class Navegacao
-    {
-        public static string ImprimirMenu()
-        {
-            string opcao = "";
-            do
-            {
-                Console.WriteLine("\nCalculadora");
-                Console.WriteLine("(+) Adição");
-                Console.WriteLine("(+) Adição");
-            }
-            return opcao = Console.ReadLine();
-        }
-    }
-}
+// namespace Menu
+// {
+//     public class Navegacao
+//     {
+//         public static string ImprimirMenu()
+//         {
+//             string opcao = "";
+//             do
+//             {
+//                 Console.WriteLine("\nCalculadora");
+//                 Console.WriteLine("(+) Adição");
+//             }
+//             return opcao = Console.ReadLine();
+//         }
+//     }
+// }
